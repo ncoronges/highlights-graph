9603c1f3f5c62e251a28e25aba7a1af539e0614a
Nick Coronges <ncoronges@gmail.com>
2017-05-04 18:30:39 -0400

    mongo load

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source 'https://rubygems.org'
 gem "kindle-highlights", '>1.0', :path=>"/Users/ncoronges/Projects/highlights-graph/kindle-highlights"
+gem "mongo"
+
--- grab_highlights.rb
+++ grab_highlights.rb
@@ -1,7 +1,49 @@
 require 'bundler/setup'
 require 'kindle_highlights'
+require 'mongo'
+require 'yaml'
+require 'optparse'
+
+options = YAML.load_file('./opts.yaml')
+
+cmd_opts = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: example.rb [cmd_opts]"
+    opts.on('-r', '--rebuild', 'Rebuild') do |v| 
+      cmd_opts[:rebuild] = v 
+    end
+end.parse!
+
+p cmd_opts
 
 
-kindle = KindleHighlights::Client.new(email_address: ENV['login'], password: ENV['password'])
+# script
+
+puts "logging in #{options["amzn_email"]}.."
+
+kindle = KindleHighlights::Client.new(email_address: options["amzn_email"], password: options["amzn_password"])
 puts "loaded #{kindle.books.length}"
+
+client = Mongo::Client.new(options["mongo_url"])
+puts "connected to mongo"
+
+if (cmd_opts[:rebuild])
+    puts "rebuild: clearing collections"
+    client[:books].drop
+end
+
+collection = client[:books]
+collection.indexes.create_one({"asin": 1}, {unique: true})
+
+kindle.books.each do |key, value|
+    puts "asin #{key} title #{value}"
+    doc = {"asin":key, "title":value, "highlights":kindle.highlights_for(key) }
+    result = collection.insert_one(doc)
+    result.n
+end
+
+
+
+
+
 
